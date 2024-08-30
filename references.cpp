c66c96466ba66224db6dea2d0903296184b57226
PanshulKantan <panshulkantansrivastava@gmail.com>
2024-08-31 00:22:23 +0530

    references vs pointers

--- references.cpp
+++ references.cpp
@@ -13,5 +13,7 @@ int main(){
     2) a pointer can be changed or can be pointed to other memory addresses but reference cant change its memory address once fixed as it becomes alias
     3) it is MUST to initialise a REFERENCE but its not important (advisable though) to not initialise a pointer as it contains a garbage value if nothing initialised 
     */
-
+   // int x= 10; const int &y = x    this will not allow to change the value of x through the alias y (you can change value through x+=3, etc)
+   // const int* const y = x     this will make the alias value constant, and also we cant change the memory address of the pointer
+   // there is nothing like const int& const y = x, etc because references are already made not to modify hence its of no use so it will give compiler error
 }
