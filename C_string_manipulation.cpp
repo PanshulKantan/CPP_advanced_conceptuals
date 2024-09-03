c11a6c5a703481547a20025a3a42c9e7faa0a5bb
PanshulKantan <panshulkantansrivastava@gmail.com>
2024-09-03 17:35:14 +0530

    C string manipulation and char detection in a string

--- C_string_manipulation.cpp
+++ C_string_manipulation.cpp
@@ -31,9 +31,10 @@ int main(){
     const char *letter2 = "blabama";
     char letter3[] = "alomaba";
 
-    std::cout << std::strcmp(letter, letter2) << std::endl;     //compares lexicographically
+    std::cout << std::strcmp(letter, letter2) << std::endl;     //compares lexicographically, hence alphabetwise, if letter comes before letter2, it will print -1
+    // 0 if both lhs and rhs are equal and 1 or any positive integer if letter2 comes before letter in lexicographical order
 
-    std::cout << std::strncmp(letter, letter3, n) <<std::endl;
+    std::cout << std::strncmp(letter, letter3, n) <<std::endl;  // this gives a certain limit upto which we want to compare, hence for n =2, it will check first 2 letters (NOTE THAT COUNTING FOR THIS STARTS FROM 1 NOT 0 HENCE 2 NOT 3)
 
     std::cout << std::strchr(message1, 'a')<< std::endl;   // it points the pointer to the first 'a' in the array and prints till the end of the string
 
@@ -41,10 +42,12 @@ int main(){
     size_t count{};
 
     const char *L = message1;
-    while((L = std::strchr(L, 'l')) != nullptr){
-        int y = L - message1;
-        std::cout << "Letter 'l' found at index " << y << std::endl;
-        L++;
+    while((L = std::strchr(L, 'l')) != nullptr){    // std::strchr operator is used when we want to point a particular character in a string
+        int y = L - message1;   // hence when used, std::strchr(arrayname, 'char') it will give a pointer to that first element equal to the char assigned in the operator we want to search
+        std::cout << "Letter 'l' found at index " << y << "starting from the line " << std::strchr(L, 'l') << std::endl;    // as it is pointing to the first targetted element, std::cout will print entire string but starting from that targetted variable
+        L++;    // it is used so that strchr wont point on that same element again and hence the condition (L=STD::STRCHR(L, 'l')) is used so that it will print the next targetted char
+        // but the difference (for the index calculation) will always start from the first element hence message1 is subtracted from it and hence we had a sperate pointer to target the character
+        // because if we update the message1 pointer to point at different element, the entire index calculation will be disturbed as the original array is modified
         count++;
     }
     std::cout << "there were total " << count << " 'l' in the statement";
